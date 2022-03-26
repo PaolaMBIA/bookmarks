aeed30a43244a8749b364565f8caacb2c8550836
PaolaMBIA <64897455+PaolaMBIA@users.noreply.github.com>
2022-03-26 14:58:23 +0100

    wip

--- src/dashboard/BookmarksList.tsx
+++ src/dashboard/BookmarksList.tsx
@@ -8,12 +8,30 @@ function BookmarksList(bookmarks: BookmarksListProps) {
     <div>
       {bookmarks.bookmarks.length !== 0 &&
         bookmarks.bookmarks.map((bookmark) => (
-          <div key={bookmark.url}>
+          <div
+            key={bookmark.url}
+            style={{ display: "flex", flexWrap: "wrap", width: "100%" }}
+          >
             {bookmark.type === "photo" && (
-              <img src={bookmark.thumbnail_url} alt={bookmark.title} />
+              <div>
+                <a href={bookmark.url} target="_blank" rel="noreferrer">
+                  {bookmark.url}
+                </a>
+                <p>{bookmark.title}</p>
+                <p>{bookmark.author_name}</p>
+                <img src={bookmark.thumbnail_url} alt={bookmark.title} />
+                <p>
+                  largeur: {bookmark.width}, hauteur: {bookmark.height}
+                </p>
+              </div>
             )}
             {bookmark.type === "video" && (
-              <div dangerouslySetInnerHTML={{ __html: bookmark.html }} />
+              <div>
+                <a href={bookmark.url}>{bookmark.url}</a>
+                <p>{bookmark.title}</p>
+                <p>{bookmark.author_name}</p>
+                <div dangerouslySetInnerHTML={{ __html: bookmark?.html }} />
+              </div>
             )}
           </div>
         ))}
