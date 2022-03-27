e8d6974d95ee43787acb009caa6e551627329454
PaolaMBIA <64897455+PaolaMBIA@users.noreply.github.com>
2022-03-27 23:50:51 +0200

    fix: remove unnecessary code

--- src/dashboard/BookmarksList.tsx
+++ src/dashboard/BookmarksList.tsx
@@ -14,58 +14,55 @@ const BookmarksList = ({ bookmarks, setBookmarks }: BookmarksListProps) => {
 
   return (
     <div className="bookmark-block">
-      {bookmarks &&
-        bookmarks.map((bookmark) => (
-          <div key={bookmark.url}>
-            <p>
-              Lien:
-              <a href={bookmark.url} target="_blank" rel="noreferrer">
-                {bookmark.url}
-              </a>
-            </p>
+      {bookmarks.map((bookmark) => (
+        <div key={bookmark.url}>
+          <p>
+            Lien:
+            <a href={bookmark.url} target="_blank" rel="noreferrer">
+              {bookmark.url}
+            </a>
+          </p>
 
-            <p>Titre: {bookmark.title}</p>
-            <p>Auteur: {bookmark.author_name}</p>
-            {bookmark.type === "photo" ? (
+          <p>Titre: {bookmark.title}</p>
+          <p>Auteur: {bookmark.author_name}</p>
+          {bookmark.type === "photo" ? (
+            <>
+              <img src={bookmark.overView} alt={bookmark.title} />
+              <p>
+                largeur: {bookmark.width}, hauteur: {bookmark.height}
+              </p>
+            </>
+          ) : (
+            bookmark.type === "video" && (
               <>
-                <img src={bookmark.overView} alt={bookmark.title} />
+                {bookmark.overView && (
+                  <div
+                    dangerouslySetInnerHTML={{
+                      __html: bookmark.overView,
+                    }}
+                  />
+                )}
                 <p>
-                  largeur: {bookmark.width}, hauteur: {bookmark.height}
+                  Durée: {bookmark.duration && getDuration(bookmark.duration)}
                 </p>
               </>
-            ) : (
-              bookmark.type === "video" && (
-                <>
-                  {bookmark.overView && (
-                    <div
-                      dangerouslySetInnerHTML={{
-                        __html: bookmark.overView,
-                      }}
-                    />
-                  )}
-                  <p>
-                    Durée: {bookmark.duration && getDuration(bookmark.duration)}
-                  </p>
-                </>
-              )
-            )}
+            )
+          )}
+          <p>
+            Date de publication: il y'a {getTimeDiff(bookmark.release_time)}
+          </p>
+          {
             <p>
-              Date de publication: il y'a {getTimeDiff(bookmark.release_time)}
+              Date d'ajout dans l'application: le{" "}
+              {getDateFormat(bookmark.upload_date)[0]}{" "}
+              {getMonth(getDateFormat(bookmark.upload_date)[1])}{" "}
+              {getDateFormat(bookmark.upload_date)[2]}
             </p>
-            {
-              <p>
-                Date d'ajout dans l'application: le{" "}
-                {getDateFormat(bookmark.upload_date)[0]}{" "}
-                {getMonth(getDateFormat(bookmark.upload_date)[1])}{" "}
-                {getDateFormat(bookmark.upload_date)[2]}
-              </p>
-            }
+          }
 
-            <button onClick={() => handleDelete(bookmark.url)}>
-              Supprimer
-            </button>
-          </div>
-        ))}
+          <button onClick={() => handleDelete(bookmark.url)}>Supprimer</button>
+        </div>
+      ))}
     </div>
   );
 };
--- src/entities-interfaces/entities.tsx
+++ src/entities-interfaces/entities.tsx
@@ -1,14 +1,3 @@
-export interface Video {
-  video_id: string;
-  duration: number;
-}
-export interface Image {
-  photo_id: string;
-  flickr_type?: string;
-  height: number;
-  width: number;
-}
-
 export interface DataModel {
   video_id?: string;
   photo_id?: string;
